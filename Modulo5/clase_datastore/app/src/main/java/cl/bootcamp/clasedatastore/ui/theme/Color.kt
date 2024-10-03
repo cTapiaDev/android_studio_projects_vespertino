41a9aec916286253763360e13f55226be8927017
Carlos Tapia <ctapiadev@gmail.com>
2024-10-02 21:20:54 -0300

    clase datastore

---
+++ Modulo5/clase_datastore/app/src/main/java/cl/bootcamp/clasedatastore/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package cl.bootcamp.clasedatastore.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFF06292)
+val PurpleGrey80 = Color(0xFF0A5833)
+val Pink80 = Color(0xFF917F10)
+val BackDarkMode = Color(0xFF10283D)
+
+val Purple40 = Color(0xFF8B0619)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
