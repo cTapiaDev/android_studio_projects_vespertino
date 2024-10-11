ccc47dbb887e343e4588bdd247af94b2e534de80
Carlos Tapia <ctapiadev@gmail.com>
2024-10-10 21:26:01 -0300

    camera app

--- Modulo5/clase_app_camara/app/src/main/java/cl/bootcamp/claseappcamara/view/GalleryView.kt
+++ Modulo5/clase_app_camara/app/src/main/java/cl/bootcamp/claseappcamara/view/GalleryView.kt
@@ -1,8 +1,86 @@
 package cl.bootcamp.claseappcamara.view
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import cl.bootcamp.claseappcamara.R
+import cl.bootcamp.claseappcamara.viewModel.ScannerViewModel
+import coil.compose.rememberAsyncImagePainter
+import kotlin.contracts.contract
 
 @Composable
-fun GalleryView() {
+fun GalleryView(
+    viewModel: ScannerViewModel
+) {
+    val context = LocalContext.current
+    val scrollState = rememberScrollState()
+    val clipboard = LocalClipboardManager.current
+    var image: Any? by remember { mutableStateOf(R.drawable.icon_gallery) }
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) {
+            image = it
+            viewModel.onRecognizedText(image, context)
+        } else {
+            viewModel.showToast(context, message = "No se ha seleccionado ninguna imagen")
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                }
+                .padding(16.dp, 8.dp),
+            painter = rememberAsyncImagePainter(image),
+            contentDescription = null
+        )
 
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Text(
+            text = viewModel.recognizedText,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .clickable {
+                    clipboard.setText(AnnotatedString(viewModel.recognizedText))
+                    viewModel.showToast(context, "Copiado")
+                }
+        )
+    }
 }
+
+
