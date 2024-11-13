b949c5f77fc5dc4cef88e8cf3d7e0ea24ce18c15
Carlos Tapia <ctapiadev@gmail.com>
2024-11-12 21:05:22 -0300

    firebase parte 2

--- Modulo6/clase_app_notes_firebase/app/src/main/java/cl/bootcamp/appnotesvespertino/navigation/NavManager.kt
+++ Modulo6/clase_app_notes_firebase/app/src/main/java/cl/bootcamp/appnotesvespertino/navigation/NavManager.kt
@@ -1,11 +1,15 @@
 package cl.bootcamp.appnotesvespertino.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import cl.bootcamp.appnotesvespertino.view.login.BlankView
 import cl.bootcamp.appnotesvespertino.view.login.TabsView
+import cl.bootcamp.appnotesvespertino.view.notes.AddNoteView
+import cl.bootcamp.appnotesvespertino.view.notes.EditNoteView
 import cl.bootcamp.appnotesvespertino.view.notes.HomeView
 import cl.bootcamp.appnotesvespertino.viewModel.LoginViewModel
 import cl.bootcamp.appnotesvespertino.viewModel.NotesViewModel
@@ -22,6 +26,15 @@ fun NavManager(loginViewModel: LoginViewModel, notesViewModel: NotesViewModel) {
         }
         composable("Home") {
             HomeView(navController, notesViewModel)
+        }
+        composable("AddNote") {
+            AddNoteView(navController, notesViewModel)
+        }
+        composable("EditNote/{idDoc}", arguments = listOf(
+            navArgument("idDoc") { type = NavType.StringType }
+        )) {
+            val idDoc = it.arguments?.getString("idDoc") ?: ""
+            EditNoteView(navController, notesViewModel, idDoc)
         }
     }
 }
